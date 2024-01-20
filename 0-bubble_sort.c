ef9be5a6391a6f317a797025f94b61f11509f601
MutabPato <patrickmtbz@gmail.com>
2024-01-20 07:01:34 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,9 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	int swapped;
 
+	if ((sizeof(array) / sizeof(array[0]) < 2)
+			return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
